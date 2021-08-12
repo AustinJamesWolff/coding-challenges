0952885ef789f02a9c1699653c2e96783aa7e76b
Austin Wolff <85139314+AustinJamesWolff@users.noreply.github.com>
2021-08-11 20:47:01 -0700

    Update caesars-cipher.js

--- caesars-cipher.js
+++ caesars-cipher.js
@@ -1,1 +1,21 @@
+function rot13(str) {
 
+  // create a Regex: /\w/g
+
+  var regex = /\w/g;
+  var alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+  // create a replacement function that shifts the letter back 13 indexes
+
+  return str.replace(regex, function replacement(match) {
+      if (alphabet.indexOf(match) < 13) {
+        return alphabet[alphabet.indexOf(match) + 13];
+      }
+      else {
+        return alphabet[alphabet.indexOf(match) - 13];
+      }
+    });
+}
+
+// Test
+console.log(rot13("SERR PBQR PNZC"));
